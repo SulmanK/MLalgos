646722e04c87552d7a48f2a5b1e2636c023545a0
Sulman Khan <43801845+SulmanK@users.noreply.github.com>
2021-01-27 00:56:40 -0500

    Update app.py

--- Algos/app.py
+++ Algos/app.py
@@ -2,8 +2,8 @@ import app_knn
 import app_linreg
 import streamlit as st
 PAGES = {
-    "App1": app1,
-    "App2": app2
+    "App1": app_knn,
+    "App2": app_linreg
 }
 st.sidebar.title('Navigation')
 selection = st.sidebar.radio("Go to", list(PAGES.keys()))
