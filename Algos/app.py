65bbc48677dc0e8dfb6297c514f0495a3f635100
Sulman Khan <43801845+SulmanK@users.noreply.github.com>
2021-03-11 10:12:55 -0500

    Update app.py

--- Algos/app.py
+++ Algos/app.py
@@ -1,13 +1,16 @@
 import app_knn
 import app_linreg
 import app_nb
+import app_svm
 import streamlit as st
 PAGES = {
     "K-Nearest Neighbors": app_knn,
     "Linear Regression": app_linreg,
-    'Naive Bayes Classifier': app_nb
+    'Naive Bayes Classifier': app_nb,
+    'Support Vector Machines': app_svm
 }
 st.sidebar.title('Navigation')
 selection = st.sidebar.radio("Go to", list(PAGES.keys()))
 page = PAGES[selection]
 page.app()
+
