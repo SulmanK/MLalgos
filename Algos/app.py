e7bf7797d2af82b20ac61e85816eb6961752d4ef
Sulman Khan <43801845+SulmanK@users.noreply.github.com>
2021-03-14 13:04:57 -0400

    Update app.py

--- Algos/app.py
+++ Algos/app.py
@@ -1,11 +1,13 @@
 import app_knn
 import app_linreg
+import app_lwr
 import app_nb
 import app_svm
 import app_perceptron
 import streamlit as st
 PAGES = {
     "Linear Regression": app_linreg,
+    'Locally Weighted Regression': app_lwr,
     "K-Nearest Neighbors": app_knn,
     'Naive Bayes Classifier': app_nb,
     'Perceptron': app_perceptron,
