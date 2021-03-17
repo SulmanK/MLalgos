a81a639f1cd8304d76b41355de03f7bcd0264fc0
Sulman Khan <43801845+SulmanK@users.noreply.github.com>
2021-03-17 16:19:45 -0400

    Update app_lr.py

--- Algos/app_lr.py
+++ Algos/app_lr.py
@@ -19,7 +19,7 @@ def app():
 
   # Supervised Learning
   st.markdown(''' ### Supervised Learning
-  Firstly, we need to explain supervised learning before we move onto Locally Weighted Regression. 
+  Firstly, we need to explain supervised learning before we move onto Logistic Regression. 
   Supervised learning involves using input features (x) to predict an output (y). 
   Given a training set, we employ a learning algorithm to get our hypothesis function. 
   Then, we feed our dataset to our hypothesis function to make predictions.  The process described is presented in Figure 1.
